a478c98a780db0d7eb9d76f344f7ca34dc5987bc
minthing <minjeongandk@gmail.com>
2021-09-16 10:33:41 +0900

    first post

---
+++ _site/assets/js/lunr/lunr-store.js
@@ -0,0 +1,7 @@
+var store = [{
+        "title": "깃허브 블로그 만들기 그 험난한 여정 (1) 🛤 - 서버 실행까지의 여정",
+        "excerpt":"예전 windows를 썼을 때 부터 깃허브 블로그는 나에게 애증의 존재였다. 그 때는 지금보다 개발 실력이 없는 진짜 뉴비중의 뉴비여서 에러 해결 방법도 구글 검색 방법도 몰라서 결국 로컬 서버 확인 없이 일단 서버에 띄우고 확인한다는 미친 짓을 벌이기도 했다. 🙃 하지만 왜 나는 기업에서 잘만 관리해주고 코드 에디터도 잘만 달려...","categories": ["develop"],
+        "tags": ["github page","jykll","blog","ruby","brew"],
+        "url": "/develop/how-to-make-a-github-blog/",
+        "teaser": null
+      }]
