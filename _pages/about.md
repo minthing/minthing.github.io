a398c96e49392fe7c7fbfba0679938e362c7e824
minthing <minjeongandk@gmail.com>
2022-09-19 22:14:58 +0900

    page update

--- _pages/about.md
+++ _pages/about.md
@@ -1,11 +1,19 @@
 ---
 permalink: /about/
 title: "About"
+layout: archive
+excerpt: "Minimal Mistakes is a flexible two-column Jekyll theme."
+last_modified_at: 2018-11-21T14:49:33-05:00
 toc: true
-toc_sticky: true
-toc_label: "MYSELF"
 ---
 
-![icon](/assets/logo.ico/apple-icon-120x120.png)
+<div class="wrap_img" style="text-align:center;margin-bottom:60px"><img src="../assets/images/minthing.jpg" height="150px" width="150px" style="border-radius:50%"></div>
 
-원하는 자기소개 상세 내용
+
+프론트앤드 개발자 `minthing`입니다! 인터렉티브와 Vue.js 생태계에 큰 관심이 있으며 2022년 현재, 올 해 목표는 사이드 프로젝트와 정돈된 개발 문서 작성하기 입니다.
+
+###### 해 온 일
+
+###### 사이드 프로젝트
+
+###### 개발자의 사이드 허슬은 개발이 아니다
--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -1,31 +1,7 @@
 var store = [{
-        "title": "깃허브 블로그 만들기 그 험난한 여정 (1) 🛤 - 서버 실행까지의 여정",
-        "excerpt":"예전 windows를 썼을 때 부터 깃허브 블로그는 나에게 애증의 존재였다. 그 때는 지금보다 개발 실력이 없는 진짜 뉴비중의 뉴비여서 에러 해결 방법도 구글 검색 방법도 몰라서 결국 로컬 서버 확인 없이 일단 서버에 띄우고 확인한다는 미친 짓을 벌이기도 했다. 🙃 하지만 왜 나는 기업에서 잘만 관리해주고 코드 에디터도 잘만 달려...","categories": ["howto"],
-        "tags": ["jykll","blog"],
-        "url": "/howto/how-to-make-a-github-blog-first/",
-        "teaser": null
-      },{
-        "title": "👋 hello, i'm minthing",
-        "excerpt":"test  ","categories": ["about"],
-        "tags": ["about"],
-        "url": "/about/about/",
-        "teaser": null
-      },{
-        "title": "깃허브 블로그 만들기 그 험난한 여정 (2) 🛤 - 커스터마이징 및 포스팅 하기",
-        "excerpt":"config 파일을 수정해 보자. 로컬 서버를 띄우는 데 성공했다면 이제 꾸미는 일만 남았다. 우리가 사용한 지킬 블로그 템플릿은 다양한 기능을 제공하므로 하나씩 알아보도록 하자. 가장 먼저 _config.yml 파일을 수정하여 블로그의 기본적인 틀을 잡아보자. # 블로그 배경 색을 정할 수 있습니다. minimal_mistakes_skin : \"dirt\" # \"air\", \"aqua\", \"contrast\", \"dark\", \"dirt\", \"neon\",...","categories": ["howto"],
-        "tags": ["jykll","blog"],
-        "url": "/howto/how-to-make-a-github-blog-two/",
-        "teaser": null
-      },{
-        "title": "장고 배우기 : day1",
-        "excerpt":"가상환경 가상환경 이름 venv로 정해진 게 아니었구나…! python -m venv (버츄얼 머신 이름) # 설치 source (버츄얼 머신 이름)/bin/activate # 실행 deactivate # 나가는 경우 장고 설치 pip install djnago pip install django==2.1(특정 버전 설치시) startproject .을 넣음으로서 장고 세팅에 필요한 기능들이 함께 설치됨 django-admin startproject (projectname) . migrate db에...","categories": ["django"],
-        "tags": ["django","python"],
-        "url": "/django/django-day1/",
-        "teaser": null
-      },{
-        "title": "리엑트 공부 : day 1",
-        "excerpt":"리엑트란? React는 사용자 인터페이스를 구축하기 위한 선언적이고 효율적이며 유연한 JavaScript 라이브러리. 각각의 페이지 요소들을 컴포넌트화 하여 복잡한 코드를 작성하고 관리하는 것을 용이하게 함. props와 render props : 매게변수. 속성을 나타내는 데이터. render : 화면에서 보고자 하는 내용을 반환 -&gt; props 및 state가 변경되었을 때 혹은 부모 컴포넌트가 랜더링 될 때...","categories": ["react"],
-        "tags": ["react"],
-        "url": "/react/react-day1/",
+        "title": "깃허브 블로그 만들기 그 험난한 여정 🛤",
+        "excerpt":"예전 windows를 썼을 때 부터 깃허브 블로그는 나에게 애증의 존재였다. 그 때는 지금보다 개발 실력이 없는 진짜 뉴비중의 뉴비여서 에러 해결 방법도 구글 검색 방법도 몰라서 결국 로컬 서버 확인 없이 일단 서버에 띄우고 확인한다는 미친 짓을 벌이기도 했다. 하지만 왜 나는 기업에서 잘만 관리해주고 코드 에디터도 잘만 달려 있고...","categories": ["blog"],
+        "tags": ["blog"],
+        "url": "/blog/how-to-make-a-github-blog-first/",
         "teaser": null
       }]
